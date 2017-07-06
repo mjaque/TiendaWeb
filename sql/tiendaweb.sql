b4c824942b5912aef96542fea67c0fadd5c0632e
mjaque <mjaque@migueljaque.com>
2017-07-06 13:58:09 +0200

    Mostrar Categorías

--- sql/tiendaweb.sql
+++ sql/tiendaweb.sql
@@ -1,52 +1,217 @@
-DROP DATABASE TiendaWeb;
-CREATE DATABASE TiendaWeb;
-USE TiendaWeb;
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 06-07-2017 a las 13:27:27
+-- Versión del servidor: 5.7.18-0ubuntu0.16.04.1
+-- Versión de PHP: 7.0.18-0ubuntu0.16.04.1
 
-CREATE TABLE Cliente(
-	id INT PRIMARY KEY,
-	usuario VARCHAR(100),
-	clave VARCHAR(100),
-	nombreCompleto VARCHAR(512),
-	direccion VARCHAR(512)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-CREATE TABLE Producto(
-	id INT PRIMARY KEY,
-	nombre VARCHAR(100),
-	descripcion VARCHAR(512),
-	precio DECIMAL(5,2),
-	imagen VARCHAR(512)
-);
+--
+-- Base de datos: `TiendaWeb`
+--
+CREATE DATABASE IF NOT EXISTS `TiendaWeb` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `TiendaWeb`;
 
-CREATE TABLE Pedido (
-	id INT PRIMARY KEY,
-	fecha DATE,
-	idCliente INT,
-	FOREIGN KEY (idCliente) REFERENCES Cliente(id)
-);
+-- --------------------------------------------------------
 
-CREATE TABLE LineaPedido(
-	id INT PRIMARY KEY,
-	cantidad INT,
-	idPedido INT,
-	idProducto INT,
-	FOREIGN KEY (idPedido) REFERENCES Pedido(id),
-	FOREIGN KEY (idProducto) REFERENCES Producto(id)
-);
+--
+-- Estructura de tabla para la tabla `Categoria`
+--
 
-CREATE TABLE Categoria(
-	id INT PRIMARY KEY,
-	nombre VARCHAR(100),
-	idPadre INT,
-	FOREIGN KEY (idPadre) REFERENCES Categoria(id)
-);
+CREATE TABLE `Categoria` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `idPadre` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE Categoria_Producto(
-	idCategoria INT,
-	idProducto INT,
-	PRIMARY KEY (idCategoria, idProducto), 
-	FOREIGN KEY (idCategoria) REFERENCES Categoria(id),
-	FOREIGN KEY (idProducto) REFERENCES Producto(id)
-);
+--
+-- Volcado de datos para la tabla `Categoria`
+--
+
+INSERT INTO `Categoria` (`id`, `nombre`, `idPadre`) VALUES
+(1, 'Mediterránea', NULL),
+(2, 'Tropical', NULL),
+(3, 'Secos', NULL),
+(4, 'Escarchadas', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Categoria_Producto`
+--
+
+CREATE TABLE `Categoria_Producto` (
+  `idCategoria` int(11) NOT NULL,
+  `idProducto` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Cliente`
+--
+
+CREATE TABLE `Cliente` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(100) DEFAULT NULL,
+  `clave` varchar(100) DEFAULT NULL,
+  `nombreCompleto` varchar(512) DEFAULT NULL,
+  `direccion` varchar(512) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `LineaPedido`
+--
+
+CREATE TABLE `LineaPedido` (
+  `id` int(11) NOT NULL,
+  `cantidad` int(11) DEFAULT NULL,
+  `idPedido` int(11) DEFAULT NULL,
+  `idProducto` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Pedido`
+--
+
+CREATE TABLE `Pedido` (
+  `id` int(11) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `idCliente` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Producto`
+--
+
+CREATE TABLE `Producto` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) DEFAULT NULL,
+  `descripcion` varchar(512) DEFAULT NULL,
+  `precio` decimal(5,2) DEFAULT NULL,
+  `imagen` varchar(512) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Categoria`
+--
+ALTER TABLE `Categoria`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idPadre` (`idPadre`);
+
+--
+-- Indices de la tabla `Categoria_Producto`
+--
+ALTER TABLE `Categoria_Producto`
+  ADD PRIMARY KEY (`idCategoria`,`idProducto`),
+  ADD KEY `idProducto` (`idProducto`);
+
+--
+-- Indices de la tabla `Cliente`
+--
+ALTER TABLE `Cliente`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `LineaPedido`
+--
+ALTER TABLE `LineaPedido`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idPedido` (`idPedido`),
+  ADD KEY `idProducto` (`idProducto`);
+
+--
+-- Indices de la tabla `Pedido`
+--
+ALTER TABLE `Pedido`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idCliente` (`idCliente`);
+
+--
+-- Indices de la tabla `Producto`
+--
+ALTER TABLE `Producto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Categoria`
+--
+ALTER TABLE `Categoria`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT de la tabla `Cliente`
+--
+ALTER TABLE `Cliente`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `LineaPedido`
+--
+ALTER TABLE `LineaPedido`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `Pedido`
+--
+ALTER TABLE `Pedido`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `Producto`
+--
+ALTER TABLE `Producto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `Categoria`
+--
+ALTER TABLE `Categoria`
+  ADD CONSTRAINT `Categoria_ibfk_1` FOREIGN KEY (`idPadre`) REFERENCES `Categoria` (`id`);
+
+--
+-- Filtros para la tabla `Categoria_Producto`
+--
+ALTER TABLE `Categoria_Producto`
+  ADD CONSTRAINT `Categoria_Producto_ibfk_1` FOREIGN KEY (`idCategoria`) REFERENCES `Categoria` (`id`),
+  ADD CONSTRAINT `Categoria_Producto_ibfk_2` FOREIGN KEY (`idProducto`) REFERENCES `Producto` (`id`);
+
+--
+-- Filtros para la tabla `LineaPedido`
+--
+ALTER TABLE `LineaPedido`
+  ADD CONSTRAINT `LineaPedido_ibfk_1` FOREIGN KEY (`idPedido`) REFERENCES `Pedido` (`id`),
+  ADD CONSTRAINT `LineaPedido_ibfk_2` FOREIGN KEY (`idProducto`) REFERENCES `Producto` (`id`);
+
+--
+-- Filtros para la tabla `Pedido`
+--
+ALTER TABLE `Pedido`
+  ADD CONSTRAINT `Pedido_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `Cliente` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
