831e363987037de594e494f9d2f7c5dda1528624
miguelmanzan <miguelmanzan@gmail.com>
2017-07-12 13:53:09 +0200

    Mensaje

--- inc/bd.php
+++ inc/bd.php
@@ -12,17 +12,29 @@ class BD{
 	
 	function verListaCategorias(){
 		$html = '<ul>';
-		if (($resultSet = $this->conexion->query("SELECT nombre FROM Categoria WHERE idPadre IS NULL")) === 0)
+		if (($resultSet = $this->conexion->query("SELECT nombre, id FROM Categoria WHERE idPadre IS NULL")) === 0)
 			die('Error de Query (' . $this->conexion->errno . ') '. $this->conexion->error);
 			
 		while(($row = $resultSet->fetch_assoc()) != NULL){
-			$html .= '<li><a href="categoria.php">'.utf8_encode($row['nombre']).'</a></li>';
+			$html .= '<li><a href="categoria.php?categoria=' . $row[ 'id'] . '">'. utf8_encode($row['nombre']). '</a></li>';
 			$html .= "\n";
 		}
 		$html .= '</ul>';
 		
 		return $html;
 	}
+
+    function verTituloCategoria( $idCategoria )
+    {
+        if( ($resultSet = $this->conexion->query( "SELECT nombre FROM categoria WHERE id = $idCategoria" ) ) === 0 )
+            die('Error de Query (' . $this->conexion->errno . ') '. $this->conexion->error);
+
+        if(($row = $resultSet->fetch_assoc()) == NULL)
+        {
+            die('Error de Query (' . $this->conexion->errno . ') '. $this->conexion->error);
+        }
+        return utf8_encode( $row[ 'nombre' ] );
+    }
 }
 	
 			
